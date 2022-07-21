b15f82eb5db82bf1b752cb8b5e32ac4db7be4320
mac <mac@qed.ai>
2022-07-22 00:39:54 +0200

    added searach bar

--- api/views.py
+++ api/views.py
@@ -7,67 +7,69 @@ from .serializers import NoteSerializer
 from .utils import updateNote, getNoteDetail, deleteNote, getNotesList, createNote
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def getRoutes(request):
 
     routes = [
         {
-            'Endpoint': '/notes/',
-            'method': 'GET',
-            'body': None,
-            'description': 'Returns an array of notes'
+            "Endpoint": "/notes/",
+            "method": "GET",
+            "body": None,
+            "description": "Returns an array of notes",
         },
         {
-            'Endpoint': '/notes/id',
-            'method': 'GET',
-            'body': None,
-            'description': 'Returns a single note object'
+            "Endpoint": "/notes/id",
+            "method": "GET",
+            "body": None,
+            "description": "Returns a single note object",
         },
         {
-            'Endpoint': '/notes/create/',
-            'method': 'POST',
-            'body': {'body': ""},
-            'description': 'Creates new note with data sent in post request'
+            "Endpoint": "/notes/create/",
+            "method": "POST",
+            "body": {"body": ""},
+            "description": "Creates new note with data sent in post request",
         },
         {
-            'Endpoint': '/notes/id/update/',
-            'method': 'PUT',
-            'body': {'body': ""},
-            'description': 'Creates an existing note with data sent in post request'
+            "Endpoint": "/notes/id/update/",
+            "method": "PUT",
+            "body": {"body": ""},
+            "description": "Creates an existing note with data sent in post request",
         },
         {
-            'Endpoint': '/notes/id/delete/',
-            'method': 'DELETE',
-            'body': None,
-            'description': 'Deletes and exiting note'
+            "Endpoint": "/notes/id/delete/",
+            "method": "DELETE",
+            "body": None,
+            "description": "Deletes and exiting note",
         },
     ]
     return Response(routes)
+
 
 # /notes GET
 # /notes POST
 # /notes/<id> GET
 # /notes/<id> PUT
-# /notes/<id> DELETE  
+# /notes/<id> DELETE
 
-@api_view(['GET', 'POST'])
+
+@api_view(["GET", "POST"])
 def getNotes(request):
 
-    if request.method == 'GET':
+    if request.method == "GET":
         return getNotesList(request)
 
-    if request.method == 'POST':
+    if request.method == "POST":
         return createNote(request)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
+@api_view(["GET", "PUT", "DELETE"])
 def getNote(request, pk):
 
-    if request.method == 'GET':
+    if request.method == "GET":
         return getNoteDetail(request, pk)
 
-    if request.method == 'PUT':
+    if request.method == "PUT":
         return updateNote(request, pk)
 
-    if request.method == 'DELETE':
+    if request.method == "DELETE":
         return deleteNote(request, pk)
--- mynotes_frontend/src/pages/NotesListPage.js
+++ mynotes_frontend/src/pages/NotesListPage.js
@@ -1,11 +1,11 @@
 import React, {useState, useEffect} from 'react'
 import ListItem from '../components/ListItem'
 import AddButton from '../components/AddButton'
-import SearchBar from '../components/SearchBar'
 
 const NotesListPage = () => {
     
     let [notes, setNotes] = useState([])
+    const [filteredData, setFilteredData] = useState([])
 
     useEffect(() => {
         getNotes() 
@@ -16,18 +16,47 @@ const NotesListPage = () => {
         let data = await response.json()
         setNotes(data)
     }
+
+    let getTitle = (note) => {
+        let title = note.body.split("\n")[0]
+        if(title.length > 45){
+          return title.slice(0, 45)
+        }
+        return title
+      }
+
+    const handleFilter = (event) =>{
+        const searchWord = event.target.value
+        const newFilter = notes.filter((value) =>{
+            return getTitle(value).toLowerCase().includes(searchWord.toLowerCase())
+        })
+        setFilteredData(newFilter)
+  }
  
     return (
-        <div className="notes">
+        <div className="notes"> 
             <div className="notes-header">
                 <h2 className="notes-title">&#9782; Notes</h2>
-                <SearchBar placeholder="Search for title..." data={notes}/> 
+                <h1 className='searchInputs'>  
+                    <input type="text" placeholder="Search for title..." onChange={handleFilter}></input>
+                </h1>
                 <p className="notes-count">{notes.length}</p>
-            </div>  
-            <div className="notes-list">
-                {notes.map((note, index)=>(
-                    <ListItem key={index} note={note}/>
-                ))}
+            </div>
+            <div>
+                {filteredData.length != 0 && (
+                <div className="notes-list">
+                    {filteredData.map((note, index)=>(
+                        <ListItem key={index} note={note}/>
+                    ))}
+                </div>
+                )}
+                {filteredData.length == 0 && (
+                <div className="notes-list">
+                    {notes.map((note, index)=>(
+                        <ListItem key={index} note={note}/>
+                    ))}
+                </div>
+                )}
             </div>
             <div><AddButton/></div>
         </div>
